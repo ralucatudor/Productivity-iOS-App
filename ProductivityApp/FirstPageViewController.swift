762590f9dd0cc647da3b0b3ea489a9504121517f
Raluca <tudorr30@gmail.com>
2022-04-08 16:34:53 +0300

    Integrate navigation, add colors to table view

---
+++ ProductivityApp/FirstPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstPageViewController.swift
+//  ProductivityApp
+//
+//  Created by Raluca Tudor on 08.04.2022.
+//
+
+import UIKit
+
+class FirstPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do additional setup after loading the view.
+        title = "Productivity App"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
